fc5b181a1eafd3849047fe41d437ec46d20530e1
K9i-0 <geukfyger@gmail.com>
2022-02-22 08:42:38 +0900

    Add: Flutter 2.10.2

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone https://github.com/flutter/flutter.git --depth 1 -b 2.10.1 "$GITHUB_WORKSPACE/_flutter"
+git clone https://github.com/flutter/flutter.git --depth 1 -b 2.10.2 "$GITHUB_WORKSPACE/_flutter"
 echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
